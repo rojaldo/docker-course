869a1f75f5b547c9375d0613b287158994b37cd5
Daniel Rojo <danielrojo@gmail.com>
2025-09-26 12:59:48 +0200

    ex_2 update

--- k8s/ex_2/app.py
+++ k8s/ex_2/app.py
@@ -10,4 +10,5 @@ def hello():
     return f"hello {my_var}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8081)
+    
---
+++ k8s/ex_2/comandos.md
@@ -0,0 +1,6 @@
+#Comandos de instalación y ejecución
+
+```bash
+pip install -r requirements.txt
+python app.py
+```
